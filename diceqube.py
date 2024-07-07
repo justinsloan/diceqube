ab575886ae0837dc4343c66744038276162ce495
justin <me@private.mail>
2024-07-06 16:10:31 -1000

    init

--- diceqube.py
+++ diceqube.py
@@ -1,11 +1,12 @@
 """
-Diceqube - Quantum Diceware
+Diceqube - Quantum Random Passwords
 The most secure password generator on the planet.
 
 Requires Python 3.6 or better
 
 Features:
-    - Generate completely random, 100% unique passphrases
+    - Strong but easily memorable passphrases
+    - Generated from 100% unique quantum random data
 
 
 Version History:
@@ -19,18 +20,22 @@ __version__ = "24.05"
 __date__ = "4 MAY 2024"
 __author__ = "Justin M. Sloan"
 
-#import random
-import time
+# Importing necessary libraries
+from importlib import resources
+from fileinput import lineno
 import json
 import requests
 import configparser
-import importlib.resources
-import os
+
 
 # Specify the location of the word list inside the package
 RESOURCE_NAME = __package__
 PATH = "word_list.txt"
-WORD_LIST_FILE = importlib.resources.path(RESOURCE_NAME, PATH)
+WORD_LIST_FILE = resources.path(RESOURCE_NAME, PATH)
+
+# Set count defaults
+PHRASE_COUNT = 5
+WORD_COUNT = 5
 
 # Load API key from file
 # Get a free API key at https://quantumnumbers.anu.edu.au/
@@ -41,13 +46,12 @@ paid_api_key = config['APIKEY']['paid'] # Unlimited requests/month for $0.005/re
 
 
 # ANU QRDG server URL
-API_URL="https://api.quantumnumbers.anu.edu.au/"
+API_URL="https://api.quantumnumbers.anu.edu.au"
 
 # Set mode options
-VERBOSE = True #bool(args.verbose) # Sets VERBOSE to True if arg is provided, False if not
-#LOCAL = False
+VERBOSE = True
 
-# Load the standard wordlist file
+# Load the standard wordlist file as dict
 WORD_DICT = {}
 with open(WORD_LIST_FILE) as f:
     for line in f.readlines():
@@ -55,80 +59,163 @@ with open(WORD_LIST_FILE) as f:
         WORD_DICT[int(index)] = word
 
 
-def fetch_AQN(api_key=free_api_key):
+def diceqube():
+    '''Generate a list of N passwords and display them to the user'''
+
+    # Get the time so we can calculate how long it takes
+    #start_time = time.time()
+
+    #time_stamp = __get_last_execution_timestamp()
+    #time_delta = int(time.time() - time_stamp)
+    #__verbose(f"Last execution: {time_delta} seconds ago.")
+
+    print("Requesting entropy, please wait...")
+
+    blocks = calculate_entropy(PHRASE_COUNT, WORD_COUNT)
+    entropy = get_entropy(blocks)
+    password_list = []
+
+    # Loop until requested number of passphrases are generated
+    for i in range(0, PHRASE_COUNT):
+        entropy, password = generate_password(entropy, WORD_COUNT)
+        password_list.append(password)
+        print(f"{i}: {password}")
+
+    # Calculate how long it took and print if Verbose mode is on
+    #run_time = int((time.time() - start_time) * 10) / 10
+    #__verbose(f"--- Finished in {run_time} seconds ---")
+
+    return password_list
+
+
+def __get_block(entropy):
+    block = iter(entropy)
+    try:
+        return next(block)
+    except StopIteration:
+        print("Error: end of block list.")
+
+
+def get_entropy(blocks_count=1):
     """
-    Fetches quantum data from ANU AQN service and returns 
+    Collect entropy from the ANU QRNG and return a list of 4-digit
+    hexadecimal numbers
+    - we need a way to limit requests to once per minute
+    -- environment variables?
+    -- pause function that counts down time if needed?
+    """
+    #__verbose(f"Getting {blocks_count} blocks of entropy...")
+    response = fetch_AQN(blocks_count=blocks_count)
+
+    #__verbose("The request was successful!")
+    #__save_execution_timestamp()
+    request = json.loads(response.text)
+    blocks = request['data']
+    count_check = len(blocks)
+    if count_check == blocks_count:
+        #__verbose("Block count validated.")
+        combined_entropy = ''.join(blocks)
+        entropy = [combined_entropy[i:i+4] for i in range(0, len(combined_entropy), 4)]
+        #__verbose(entropy)
+
+        return entropy
+    else:
+        #__save_execution_timestamp()
+        print(f"Error: {blocks_count} block(s) were requested but {count_check} blocks were returned.")
+
+
+def calculate_entropy(phrase_count=1, word_count=5):
+    """
+    Calculate how much entropy data is needed and return an int of
+    how many blocks to request
+    Formula: Num_Blocks = (Words * 5) * Num_Phrases
+    - minimum block request is 1
+    - we can get 512 rolls per block
+    - each word requires 5 rolls
+    """
+    # sanitize input
+    phrase_count = int(phrase_count) # truncate
+    word_count = int(word_count) # truncate
+
+    # do some error checking
+    if phrase_count < 1 or word_count < 1:
+        print(f"Error {lineno}: Word Count ({word_count}) or Phrase Count ({phrase_count}) cannot be less than 1.")
+        exit(1)
+    elif phrase_count > 100:
+        print(f"Error {lineno}: Phrase Count cannot be greater than 100.")
+        exit(1)
+    elif word_count > 12:
+        print(f"Error {lineno}: Word Count cannot be greater than 30.")
+        exit(1)
+
+    # do the math
+    roll_count = (word_count * 6) * phrase_count # we need six rolls per word
+    block_count = roll_count / 10
+    blocks = int(block_count + 1) # truncate and round up
+    #__verbose(f"Blocks calculated: {blocks}")
+
+    return blocks
+
+
+def generate_password(entropy, word_count=5, char="-", pre="", post=""):
+    """
+    Generate a single password and return as a string
+    """
+    dice_words = []
+    dice = []
+    numbers = []
+
+    block_count = word_count * 5              # get enough blocks for the number of words
+    blocks = entropy[:block_count]            # slice block_count items from entropy
+    del entropy[:block_count]                 # delete the sliced items from entropy
+
+    rand_num_count = word_count               # get enough blocks for use a base10 numbers
+    num_blocks = entropy[:rand_num_count]     # slice rand_num_count items from entropy
+    del entropy[:rand_num_count]              # delete the sliced items from entropy
+
+    for block in blocks:                      # convert hexadecimal blocks to mod6
+        die = int(block, 16) % 6 + 1
+        dice.append(str(die))
+
+    for num in num_blocks:                    # convert hexadecimal blocks to mod99
+        number = int(num, 16) % 99
+        numbers.append(str(number))
+
+    dice = ''.join(dice)
+    roll = [str(dice)[i:i + 5] for i in range(0, len(str(dice)), 5)]
+    num_loop = 0
+    for i in roll:
+        #__verbose(f"Dice Rolls: {i}")
+        # TODO make capitalization an arg option
+        token = WORD_DICT[int(i)]
+        rand_number = numbers[num_loop]
+        num_loop += 1
+        #__verbose(str(token + rand_number))
+        word = token[0].upper() + token[1:] + str(rand_number)
+        dice_words.append(word)
+
+    password = pre + char.join(dice_words) + post
+
+    return entropy, password
+
+
+def fetch_AQN(api_key=free_api_key, blocks_count=1):
+    """
+    Fetches quantum data from ANU AQN service and returns
     it as a list of integers. Each request can produce up
     to 170 diceware words.
     """
     headers = {'Content-Type': 'application/json',
                'x-api-key': api_key,
               }
-    response = requests.get(f"{API_URL}?length=1020&type=uint16", headers=headers)
+    response = requests.get(f"{API_URL}?length={blocks_count}&type=hex16&size=10", headers=headers)
 
-    while response.status_code == 500:
+    if not response.status_code == 200:
         if not paid_api_key:
-            print("Error: There are no remaining free requests")
+            print(f"Error {response.status_code}: There are no remaining free requests")
         else:
             response = fetch_AQN(paid_api_key)  # change key and try again
 
     return response
 
 
-def generate_diceware_phrase(data, phrase_count=5, word_count=6, char="-", pre="", post=""):
-    """Generates passphrases"""
-    qube_i = 0
-    for phrase in range(0, phrase_count):
-        phrase_words = []
-
-        for word in range(0, word_count):
-            token = ""
-
-            for i in range(5):
-                roll = data[qube_i] % 6 + 1
-                qube_i += 1
-                token += str(roll)
-
-            #p_verbose(f"Dice Roll: {token}")
-            phrase_words.append(WORD_DICT[int(token)])
-
-        passphrase = pre + char.join(phrase_words) + post
-        return passphrase
-
-
-def validate_request(phrases, words):
-    """
-    Validates the request is within the maximum range (170)
-    and returns a boolean.
-    """
-    word_count = phrases * words
-    if word_count > 170:
-        max = int(170 / words)
-        print(f"Error: With {words} words per passphrase, you can only generate {max} passphrases per request.")
-        return False
-    return True
-
-
-def main():
-    """Takes optional arguments and prints passphrases."""
-    # Get the time so we can calculate how long it takes
-    start_time = time.time()
-
-    valid = validate_request(1, 5)
-
-    if not valid:
-        exit()
-
-    #p_verbose("Gathering quantum data...")
-    qube = fetch_AQN()
-
-    if qube.status_code == 200:
-        content = qube.text
-        load = json.loads(content)
-        data = load["data"]
-        generate_diceware_phrase(data, 1, 5, "-")
-    else:
-        print(f"Error: {qube.status_code}, {qube.text}")
-
-    # Calculate how long it took and print if Verbose mode is on
-    #p_verbose(f"--- {time.time() - start_time} seconds ---")
--- flask_app.py
+++ flask_app.py
@@ -1,136 +1,15 @@
-# Importing necessary libraries
+from .diceqube import diceqube
 from flask import Flask
 from flask import render_template
-import string
-import secrets
-import json
-import requests
-import configparser
-import pkg_resources
 
-# Specify the location of the word list inside the package
-RESOURCE_NAME = __name__
-PATH = "word_list.txt"
-WORD_LIST_FILE = pkg_resources.resource_filename(RESOURCE_NAME, PATH)
-
-# Load API key from file
-# Get a free API key at https://quantumnumbers.anu.edu.au/
-config = configparser.ConfigParser()
-config.read('config.txt')
-free_api_key = config['APIKEY']['free'] # 100 requests/month for free
-paid_api_key = config['APIKEY']['paid'] # Unlimited requests/month for $0.005/request
-
-
-# ANU QRDG server URL
-API_URL="https://api.quantumnumbers.anu.edu.au/"
-
-# Set mode options
-VERBOSE = True #bool(args.verbose) # Sets VERBOSE to True if arg is provided, False if not
-#LOCAL = False
-
-# Load the standard wordlist file
-WORD_DICT = {}
-with open(WORD_LIST_FILE) as f:
-    for line in f.readlines():
-        index, word = line.strip().split('\t')
-        WORD_DICT[int(index)] = word
-
-# start the flask application
+# instantiate flask application
 app = Flask(__name__)
 
 
 @app.route('/')  # Define route for root URL
-def diceqube():
-    '''Generate a list of 5 passwords and display them to the user'''
-
-    valid = validate_request(args.count, args.words)
-
-    if not valid:
-        exit()
-
-    # p_verbose("Gathering quantum data...")
-    qube = fetch_AQN()
-
-    if qube.status_code == 200:
-        content = qube.text
-        load = json.loads(content)
-        data = load["data"]
-        password_list = [generate_diceware_phrase(data, args.count, args.words, args.char, args.pretext, args.posttext) for _ in range(1)]
-    else:
-        password_list = f"Error: {qube.status_code}, {qube.text}"
-
+def index():
+    password_list = diceqube()
     return render_template('index.html', password_list=password_list)
-
-def generate_random_password():
-    '''Generate a random password of 12 characters (8 alphabets, 4 digits).'''
-
-    alphabet = string.ascii_letters  # 26 lowercase + 26 uppercase letters
-    numbers = string.digits  # 0 to 9 digits
-    all_chars = alphabet + numbers  # Combine both alphabets and numbers
-
-    while True:
-        password = ''.join(secrets.choice(all_chars) for i in range(8))
-        password += ''.join(secrets.choice(numbers) for i in range(4))
-
-        if any(c.islower() for c in password) and any(c.isupper() for c in password):
-            break  # The randomly generated password has at least one uppercase & lowercase letter, 8 alphabets + 4 digits
-
-    return password
-
-
-def generate_diceware_phrase(data, phrase_count=5, word_count=6, char="-", pre="", post=""):
-    """Generates passphrases"""
-    qube_i = 0
-    for phrase in range(0, phrase_count):
-        phrase_words = []
-
-        for word in range(0, word_count):
-            token = ""
-
-            for i in range(5):
-                roll = data[qube_i] % 6 + 1
-                qube_i += 1
-                token += str(roll)
-
-            #p_verbose(f"Dice Roll: {token}")
-            phrase_words.append(WORD_DICT[int(token)])
-
-        passphrase = pre + char.join(phrase_words) + post
-        return passphrase
-
-
-def fetch_AQN(api_key=free_api_key):
-    """
-    Fetches quantum data from ANU AQN service and returns
-    it as a list of integers. Each request can produce up
-    to 170 diceware words.
-    """
-    headers = {'Content-Type': 'application/json',
-               'x-api-key': api_key,
-              }
-    response = requests.get(f"{API_URL}?length=1020&type=uint16", headers=headers)
-
-    while response.status_code == 500:
-        if not paid_api_key:
-            print("Error: There are no remaining free requests")
-        else:
-            response = fetch_AQN(paid_api_key)  # change key and try again
-
-    return response
-
-
-def validate_request(phrases, words):
-    """
-    Validates the request is within the maximum range (170)
-    and returns a boolean.
-    """
-    word_count = phrases * words
-    if word_count > 170:
-        max = int(170 / words)
-        print(f"Error: With {words} words per passphrase, you can only generate {max} passphrases per request.")
-        return False
-    return True
-
 
 # Run the Flask app if this script is executed directly
 if __name__ == '__main__':
